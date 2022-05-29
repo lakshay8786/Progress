efe5ef7aad3409dd6da945f41cb3da6d08c99013
lakshay8786 <flipylakshay790@gmail.com>
2022-05-29 15:58:59 +0530

    first commit

---
+++ src/Progress.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProgressBar = ({value,max}) => {
+    return(
+        <progress value = {value}  max={max}/>
+    )
+}
+ProgressBar.propTypes = {
+    value: PropTypes.number.isRequired,
+    max:PropTypes.number,
+};
+
+ProgressBar.defaultProps = {
+    max:4
+}
+
+export default ProgressBar;
